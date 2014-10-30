ea58db70f3af735d02ae45657a019bb372a58355
Ted Lawless <lawlesst@gmail.com>
2014-10-30 14:32:21 -0400

    Add support for marc-in-json.

--- spec/lib/blacklight_solr_document_marc_spec.rb
+++ spec/lib/blacklight_solr_document_marc_spec.rb
@@ -31,6 +31,16 @@ describe "Blacklight::Solr::Document::Marc" do
     end
   end
 
+  describe "marc json mode" do
+    before(:each) do
+      @mock_class.extension_parameters[:marc_format_type] = :json
+    end
+    it "should read and parse a marc json file" do
+      document = @mock_class.new(:marc => sample_marc_json)
+      document.to_marc.should == marc_from_string(:json => sample_marc_json)
+    end
+  end
+
   it "should register all its export formats" do
     document = @mock_class.new
     Set.new(document.export_formats.keys).should  be_superset(Set.new([:marc, :marcxml, :openurl_ctx_kev, :refworks_marc_txt, :endnote, :xml]))    
@@ -75,6 +85,11 @@ describe "Blacklight::Solr::Document::Marc" do
   def sample_marc_binary
     "00386pam a22001094a 4500001000900000003000600009008004100015245008900056260005400145700004500199700003200244\036a4802615\036SIRSI\036020828s2003    enkaf    b    001 0 eng  \03600\037aApples :\037bbotany, production, and uses /\037cedited by D.C. Ferree and I.J. Warrington.\036  \037aOxon, U.K. ;\037aCambridge, MA :\037bCABI Pub.,\037cc2003.\0361 \037aFerree, David C.\037q(David Curtis),\037d1943-\0361 \037aWarrington, I. J.\037q(Ian J.)\036\035"
   end
+
+  def sample_marc_json
+    "{\"leader\":\"00386pam a22001094a 4500\",\"fields\":[{\"001\":\"a4802615\"},{\"003\":\"SIRSI\"},{\"008\":\"020828s2003    enkaf    b    001 0 eng  \"},{\"245\":{\"ind1\":\"0\",\"ind2\":\"0\",\"subfields\":[{\"a\":\"Apples :\"},{\"b\":\"botany, production, and uses /\"},{\"c\":\"edited by D.C. Ferree and I.J. Warrington.\"}]}},{\"260\":{\"ind1\":\" \",\"ind2\":\" \",\"subfields\":[{\"a\":\"Oxon, U.K. ;\"},{\"a\":\"Cambridge, MA :\"},{\"b\":\"CABI Pub.,\"},{\"c\":\"c2003.\"}]}},{\"700\":{\"ind1\":\"1\",\"ind2\":\" \",\"subfields\":[{\"a\":\"Ferree, David C.\"},{\"q\":\"(David Curtis),\"},{\"d\":\"1943-\"}]}},{\"700\":{\"ind1\":\"1\",\"ind2\":\" \",\"subfields\":[{\"a\":\"Warrington, I. J.\"},{\"q\":\"(Ian J.)\"}]}}]}"
+  end
+
   def marc_from_string(args = {})
     if args[:binary]
       reader = MARC::Reader.new(StringIO.new(args[:binary]))
@@ -82,6 +97,9 @@ describe "Blacklight::Solr::Document::Marc" do
     elsif args[:xml]
       reader = MARC::XMLReader.new(StringIO.new(args[:xml]))
       reader.each {|rec| return rec }
+    elsif args[:json]
+      rec = MARC::Record.new_from_hash(JSON.parse(args[:json]))
+      return rec
     end
     return nil
   end
