e1d20fae8b5ab04154f252e2c81d1bdaf82ff1e0
Chris Beer <cabeer@stanford.edu>
2015-11-08 08:59:58 -0800

    Remove deprecated code

--- app/helpers/blacklight_marc_helper.rb
+++ app/helpers/blacklight_marc_helper.rb
@@ -2,15 +2,6 @@ module BlacklightMarcHelper
 
   # This method should move to BlacklightMarc in Blacklight 6.x
   def refworks_export_url params = {}
-    if params.is_a? ::SolrDocument or (params.nil? and instance_variable_defined? :@document)
-      Deprecation.warn self, "Calling #refworks_export_url without a :url is deprecated. Pass in e.g. { url: url_for_document(@document, format: :refworks_marc_txt) } instead"
-      url = url_for_document(params || @document)
-      params = { url: polymorphic_url(url, format: :refworks_marc_txt, only_path: false) }
-    elsif params[:id]
-      Deprecation.warn self, "Calling #refworks_export_url without a :url is deprecated. Pass in e.g. { url: url_for_document(@document, format: :refworks_marc_txt) } instead"
-      params = { url: polymorphic_url(url_for_document(params), format: :refworks_marc_txt, only_path: false) }
-    end
-
     "http://www.refworks.com/express/expressimport.asp?vendor=#{CGI.escape(params[:vendor] || application_name)}&filter=#{CGI.escape(params[:filter] || "MARC Format")}&encoding=65001" + (("&url=#{CGI.escape(params[:url])}" if params[:url]) || "")
   end
 
--- lib/generators/blacklight_marc/marc_generator.rb
+++
@@ -1,9 +0,0 @@
-module BlacklightMarc
-  class MarcGenerator < Rails::Generators::Base
-
-    def generate_blacklight_marc_install
-      say "blacklight_marc:marc generator is deprecated; use blacklight:marc:install instead", :red
-      generate "blacklight:marc:install"
-    end
-  end
-end
