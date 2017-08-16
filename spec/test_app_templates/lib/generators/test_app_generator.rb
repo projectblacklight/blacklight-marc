34a2f4dbb96a1e38e7eda44dd9b98dac3f5e471d
Justin Coyne <jcoyne@justincoyne.com>
2017-08-16 12:54:44 -0500

    Depend on Blacklight 7.0.0.alpha

--- spec/test_app_templates/lib/generators/test_app_generator.rb
+++ spec/test_app_templates/lib/generators/test_app_generator.rb
@@ -1,35 +1,29 @@
 require 'rails/generators'
 
 class TestAppGenerator < Rails::Generators::Base
-  def fix_travis_rails_4 
+  def fix_travis_rails_4
     if ENV['TRAVIS']
-      insert_into_file 'app/assets/stylesheets/application.css', :before =>'/*' do      
+      insert_into_file 'app/assets/stylesheets/application.css', :before =>'/*' do
         "@charset \"UTF-8\";\n"
       end
     end
   end
 
-  def remove_index 
+  def remove_index
     remove_file "public/index.html"
   end
-  
-  def run_blacklight_generator
-    say_status("warning", "GENERATING BL", :yellow) 
-    gem 'blacklight'
-    gem "traject", '~>2.0'
-    Bundler.with_clean_env do
-      run "bundle install"
-    end
 
+  def run_blacklight_generator
+    say_status("warning", "GENERATING BL", :yellow)
     generate 'blacklight:install', '--devise'
   end
 
   def run_test_support_generator
-    say_status("warning", "GENERATING test_support", :yellow)       
+    say_status("warning", "GENERATING test_support", :yellow)
 
     generate 'blacklight:test_support'
   end
-  
+
   def run_blacklight_marc_generator
     generate 'blacklight:marc:install'
   end
