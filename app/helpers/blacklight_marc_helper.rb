811ea6c55615a7cdcfbc54965b40fe2f251afe6e
Charlie Morris <cdm32@psu.edu>
2020-06-11 19:55:19 -0400

    Accounts for the second argument that ActionDispatch::Routing::PolymorphicRoutes.polymorphic_method sends in Rails 6

--- app/helpers/blacklight_marc_helper.rb
+++ app/helpers/blacklight_marc_helper.rb
@@ -1,18 +1,18 @@
 module BlacklightMarcHelper
 
   # This method should move to BlacklightMarc in Blacklight 6.x
-  def refworks_export_url params = {}
+  def refworks_export_url params = {}, *_
     "http://www.refworks.com/express/expressimport.asp?vendor=#{CGI.escape(params[:vendor] || application_name)}&filter=#{CGI.escape(params[:filter] || "MARC Format")}&encoding=65001" + (("&url=#{CGI.escape(params[:url])}" if params[:url]) || "")
   end
 
-  def refworks_solr_document_path opts = {}
+  def refworks_solr_document_path opts = {}, *_
     if opts[:id]
       refworks_export_url(url: solr_document_url(opts[:id], format: :refworks_marc_txt))
     end
   end
 
   # For exporting a single endnote document. (endnote_catalog_path is defined by blacklight-marc and it is used for multiple document export)
-  def single_endnote_catalog_path opts = {}
+  def single_endnote_catalog_path opts = {}, *_
     solr_document_path(opts.merge(format: 'endnote'))
   end
 
