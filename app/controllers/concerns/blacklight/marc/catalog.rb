8d27d5f11ad1e2ee08e63a30865da553d83a8775
Chris Beer <chris@cbeer.info>
2021-12-11 09:20:29 -0800

    Move the catalog controller overrides into a controller concern

--- app/controllers/concerns/blacklight/marc/catalog.rb
+++ app/controllers/concerns/blacklight/marc/catalog.rb
@@ -9,13 +9,13 @@ module Blacklight::Marc
     end
 
     def librarian_view
-      @response, deprecated_document = search_service.fetch params[:id]
-      @document = ActiveSupport::Deprecation::DeprecatedObjectProxy.new(deprecated_document, "The @document instance variable is deprecated and will be removed in Blacklight-marc 8.0")
+      @document = search_service.fetch(params[:id])
+
       respond_to do |format|
         format.html do
           return render layout: false if request.xhr?
           # Otherwise draw the full page
-        end 
+        end
       end
     end
 
